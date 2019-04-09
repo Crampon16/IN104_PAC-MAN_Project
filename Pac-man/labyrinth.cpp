8be710a0807bc7792dbcdb837736f61d84826ad3
Crampon16 <48726144+Crampon16@users.noreply.github.com>
2019-04-09 17:02:18 +0200

    Update labyrinth.cpp
    Added the beginning of a stage creator function (from a file).

--- Pac-man/labyrinth.cpp
+++ Pac-man/labyrinth.cpp
@@ -7,3 +7,61 @@
 //
 
 #include "labyrinth.hpp"
+
+using namespace std;
+
+//string splitting function
+void split(const string &s, char delim, vector<string> &elems) {
+    stringstream ss(s);
+    string item;
+    while (getline(ss, item, delim)) {
+        elems.push_back(item);
+    }
+}
+
+Stage init_stage(string path)
+{
+    //initializes an empty Stage
+    Stage stage;
+    Square empty_square;
+    for (int i = 0; i < STAGE_HEIGHT; ++i)
+    {
+        vector<Square> empty_line;
+        stage.matrix.push_back(empty_line);
+        for (int j = 0; j < STAGE_WIDTH; ++j)
+        {
+            stage.matrix[i].push_back(empty_square);
+        }
+    }
+    
+    //open stream to read layout file
+    ifstream stream(path);
+    if(not stream)
+    {
+        cout << "ERROR: Unable to open the stage layout file." << endl;
+        return stage;
+    }
+    string buffer;
+    vector<string> params;
+    
+    int i = 0;
+    while ( getline(stream, buffer) )
+    {
+        for(int j = 0; j < STAGE_WIDTH; ++j)
+        {
+            switch (buffer[j])
+            {
+                case 'X':
+                    stage.matrix[i][j].obstructed = true;
+                    break;
+                case 'O':
+                    stage.matrix[i][j].item = "gum";
+                default:
+                    break;
+            }
+        }
+        ++i;
+    }
+    
+    return stage;
+}
