73ef84516b9725fb9b92dbbaaf6ca015eb01e4ac
Crampon16 <48726144+Crampon16@users.noreply.github.com>
2019-04-18 23:22:16 +0200

    Added blinky and loads the extended level layout

--- Pac-man/main.cpp
+++ Pac-man/main.cpp
@@ -181,11 +181,24 @@ int main(int argv, char** args)
             }
              */
             
-            Stage stage = init_stage("layout.txt");
+            Stage stage = init_stage("layout2.txt");
             Entity pac( {3*SQUARE_SIZE/2, 3*SQUARE_SIZE/2}, 250, std::stack<std::pair<int,int>>() , pacman_AI, 0);
             stage.entities.push_back(pac);
             stage.entities_positions.push_back({1,1});
             
+            Entity blinky( {3*SQUARE_SIZE/2, 3*SQUARE_SIZE/2}, 125, std::stack<std::pair<int,int>>() , blinky_AI, 1);
+            stage.entities.push_back(blinky);
+            stage.entities_positions.push_back({1,1});
+            
+            cout << SDL_GetTicks() << endl;
+            stack<pair<int, int>> path = bfs(stage, {1,1}, {11,11});
+            cout << SDL_GetTicks() << endl;
+            
+            while (not path.empty())
+            {
+                cout << path.top().first << " " << path.top().second << endl;
+                path.pop();
+            }
             
             SDL_Event e;
             bool quit = false;
@@ -209,8 +222,8 @@ int main(int argv, char** args)
                     stage.entities[i].move(17, stage);
                 }
                 display(renderer, stage);
-                cout << "x:" << stage.entities[0].get_position().x << endl;
-                cout << "y:" << stage.entities[0].get_position().y << endl;
+                //cout << "x:" << stage.entities[0].get_position().x << endl;
+                //cout << "y:" << stage.entities[0].get_position().y << endl;
                 
                 cap.cap();
             }
