98a333b034f19fefc20b13a1964741cadbb5be58
Luckijig <48726156+Luckijig@users.noreply.github.com>
2019-04-23 17:31:38 +0200

    adding state

--- Pac-man/entity.hpp
+++ Pac-man/entity.hpp
@@ -15,7 +15,7 @@
 #include <stack>
 #include <math.h>
 
-#include "SDL2/SDL.h"
+#include "SDL.h"
 
 #include "labyrinth.hpp"
 #include "texture_wrapper.hpp"
@@ -23,21 +23,31 @@
 
 struct Stage;
 
+enum entity_state
+{
+    NORMAL,
+    KILLER,
+    AFRAID,
+    DEAD
+};
+
 class Entity
 {
 public:
-    
+
     Entity(SDL_Point position, int speed, std::stack<std::pair<int,int> > path,
            std::stack<std::pair<int,int> > (*path_finder)(std::pair<int, int>, Stage&), int Id);
-    
+
     SDL_Point get_position();
     void set_position(std::pair<int, int> square, Stage& stage);
     void set_position(SDL_Point pos, Stage& stage);
-    
+
     std::vector<SDL_Rect> get_colliders();
-    
+
     std::stack<std::pair<int, int>> get_path();
     void set_path(std::stack<std::pair<int,int>> path);
+
+    entity_state state;
 
     /*
      Updates position: "postion += delta*current_speed"
@@ -46,33 +56,33 @@ public:
      If path is empty, calls find_path().
      */
     void move(int delta, Stage& stage);
-    
+
     /*
      Calls path_finder() to find what new path is to be taken, then changes path and current_speed
      to do so.
      */
     void find_path(Stage& stage);
-    
+
     void set_path_finding(std::stack<std::pair<int,int> > (*path_finder)(std::pair<int, int>, Stage&));
-    
+
 private:
-    
+
     SDL_Point position;
     //contains every hitbox of the entity
     std::vector<SDL_Rect> colliders;
-    
+
     //how fast the Entity moves
     int speed;
-    
+
     //Entity's Id
     int Id;
-    
+
     //What Squares the Entity is going to go through.
     /*
      Once a square is reached, it is removed from the stack. Once the stack is empty, find_path() is called.
      */
     std::stack<std::pair<int,int> > path;
-    
+
     /*
      The path finding function takes the position of the Entity on the Stage and the Stage itself. It
      empties path then fills it with the new steps to take.
@@ -80,7 +90,7 @@ private:
      */
     //TODO: ask Liam to explain this syntax
     std::stack<std::pair<int,int> > (*path_finder)(std::pair<int, int>, Stage&);
-    
-    
+
+
 };
 #endif /* entity_hpp */
