a04769282a7c57328369ca04abcd42d0f3263da4
Crampon16 <48726144+Crampon16@users.noreply.github.com>
2019-04-23 15:38:36 +0200

    Defined the constants better

--- Pac-man/constants.hpp
+++ Pac-man/constants.hpp
@@ -9,14 +9,16 @@
 #ifndef constants_h
 #define constants_h
 
-//in pixels
-const int SCREEN_WIDTH = 400;
-const int SCREEN_HEIGHT = 400;
-
 //in lines and columns
-const int STAGE_WIDTH = 20, STAGE_HEIGHT = 20;
+const int STAGE_WIDTH = 28, STAGE_HEIGHT = 30;
 
 //in pixels
 const int SQUARE_SIZE = 20;
+
+//in pixels
+const int SCREEN_WIDTH = SQUARE_SIZE*STAGE_WIDTH;
+const int SCREEN_HEIGHT = SQUARE_SIZE*STAGE_HEIGHT;
+
+const int pac_man_id = 0;
 
 #endif /* constants_h */
