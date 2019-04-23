eef5c67515cb091af11a157f384e4c22ccfbbc5c
Crampon16 <48726144+Crampon16@users.noreply.github.com>
2019-04-23 15:34:44 +0200

    Updated header

--- Pac-man/labyrinth.hpp
+++ Pac-man/labyrinth.hpp
@@ -20,8 +20,17 @@
 
 #include "constants.hpp"
 #include "entity.hpp"
+#include "pathfinding.hpp"
 
 class Entity;
+
+enum entity_state
+{
+    NORMAL,
+    KILLER,
+    AFRAID,
+    DEAD
+};
 
 //A Stage is composed of a matrix of Squares
 struct Square
@@ -42,13 +51,29 @@ struct Stage
 {
     std::vector<std::vector<Square>> matrix;
     
-    //Allows pathfinding functions to quickly locate the position of other entities
+    //Allows pathfinding functions to quickly locate some positions
     std::vector<std::pair<int, int>> entities_positions;
-    
+    std::vector<std::pair<int, int>> entities_spawn_point;
+    std::vector<std::pair<int, int>> entities_spawn_direction;
     std::vector<Entity> entities;
     
+    //these variables characterize what state the game is in
     char last_key_input;
+    Uint32 killer_mode_start;
+    std::vector<entity_state> states = {NORMAL, NORMAL, NORMAL, NORMAL, NORMAL};
+    int lives = 3;
+    
+    int score = 0;
 };
 
 Stage init_stage(std::string path);
+
+/*
+ This function detects several type of collisions and applies different effects to each:
+    * gum/pac collisions destroy the gum and increase the score
+    * super gum/pac collisions destroy the gum and and turn on killer mode for some time
+    * ghost/pac collisions (out of killer mode) makes pac respawn and decreases the amount of lives remaining
+    * ghost/pac collisions (in killer mode) increases the score and set ghost to their back to spawn behaviour
+*/
+void handle_collisions(Stage& stage);
 #endif /* labyrinth_hpp */
