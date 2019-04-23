0f78ecccd4f5f5abac50c71d167f51090fcefb08
Crampon16 <48726144+Crampon16@users.noreply.github.com>
2019-04-23 15:14:20 +0200

    Changed copyright issues

--- Pac-man/texture_wrapper.cpp
+++ Pac-man/texture_wrapper.cpp
@@ -1,9 +1,8 @@
 //
 //  texture_wrapper.cpp
-//  enstaroid
+//  pac-man
 //
-//  Created by Liam Rampon on 19/01/2019.
-//  Copyright © 2019 Liam Rampon. All rights reserved.
+//  
 //
 
 #include "texture_wrapper.hpp"
