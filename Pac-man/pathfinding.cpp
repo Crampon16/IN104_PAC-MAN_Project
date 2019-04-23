89d892e71d58870c64272f17d4f729dadceae4ff
Crampon16 <48726144+Crampon16@users.noreply.github.com>
2019-04-24 00:15:18 +0200

    Lots of changes
    bfs() now returns every square in the calculated path.
    Inky's and Blinky's AIs use the process of the original game thanks to nearest_square().
    Pac's AI uses previous_square and the changes to move() in the Entity class to allow U-turns.

--- Pac-man/pathfinding.cpp
+++ Pac-man/pathfinding.cpp
@@ -138,33 +138,101 @@ SquareStack bfs(Stage& stage, SquarePos start, SquarePos goal)
     SquarePos current = nodes[goal.first][goal.second].previous;
     while(current != start)
     {
-        if(stage.matrix[current.first][current.second].is_node)
+        //if(stage.matrix[current.first][current.second].is_node)
             path.push(current);
         current = nodes[current.first][current.second].previous;
     }
     return path;
 }
 
-SquareStack blinky_AI(SquarePos pos, Stage& stage)
+int square_distance(SquarePos square1, SquarePos square2)
 {
-    return bfs(stage, pos, stage.entities_positions[0]);
+    return (square1.first - square2.first)*(square1.first - square2.first) + (square1.second - square2.second)*(square1.second - square2.second);
 }
 
-SquareStack pinky_AI(SquarePos pos, Stage& stage)
+SquareStack nearest_square(Stage& stage, SquarePos start, SquarePos goal, SquarePos previous)
+{
+    SquareStack candidates;
+    candidates.push({start.first - 1, start.second});
+    candidates.push({start.first + 1, start.second});
+    candidates.push({start.first, start.second - 1});
+    candidates.push({start.first, start.second + 1});
+    
+    //find first element of stack that can be gone through
+    SquarePos best;
+    bool found_first = false;
+    while (not found_first)
+    {
+        SquarePos candidate = candidates.top();
+        //valid square to go to
+        if (candidate != previous and not stage.matrix[candidate.first][candidate.second].obstructed)
+        {
+            found_first = true;
+            best = candidate;
+        }
+        candidates.pop();
+    }
+    
+    //compare valid elements to find the best, euclidean distance wise
+    while (not candidates.empty())
+    {
+        SquarePos candidate = candidates.top();
+        //valid square to go to
+        if (candidate != previous and not stage.matrix[candidate.first][candidate.second].obstructed)
+        {
+            //compare distance to goal
+            if (square_distance(goal, best) > square_distance(goal, candidate))
+            {
+                best = candidate;
+            }
+        }
+        candidates.pop();
+    }
+    
+    SquareStack path;
+    path.push(best);
+    return path;
+}
+
+//dummied out version using bfs
+/*
+SquareStack blinky_AI2(int id, SquarePos pos, Stage& stage)
+{
+    return bfs(stage, pos, stage.entities_positions[0]);
+}*/
+
+SquareStack blinky_AI(int id, SquarePos pos, Stage& stage)
+{
+    
+    return nearest_square(stage, pos, stage.entities_positions[0], stage.entities[id].get_previous_square());
+}
+
+//dummied out version using bfs
+/*
+SquareStack pinky_AI2(int id, SquarePos pos, Stage& stage)
 {
     SquareStack pac_path = stage.entities[0].get_path();
     
     return bfs(stage, pos, pac_path.top());
+}*/
+
+SquareStack pinky_AI(int id, SquarePos pos, Stage& stage)
+{
+    SquarePos base = stage.entities_positions[0], prev = stage.entities[0].get_previous_square();
+    SquarePos offset = {4*(base.first - prev.first), 4*(base.second - prev.second)};
+    SquarePos goal = {base.first + offset.first, base.second + offset.second};
+    
+    return nearest_square(stage, pos, goal, stage.entities[id].get_previous_square());
 }
 
-SquareStack inky_AI(SquarePos pos, Stage& stage)
+SquareStack inky_AI(int id, SquarePos pos, Stage& stage)
 {
     SquareStack stack;
     
     return stack;
 }
 
-SquareStack clyde_AI(SquarePos pos, Stage& stage)
+SquareStack clyde_AI(int id, SquarePos pos, Stage& stage)
 {
     SquareStack stack;
     
@@ -172,7 +240,7 @@ SquareStack clyde_AI(SquarePos pos, Stage& stage)
 }
 
 //basic AI for testing : only goes one way, and stops at the edge of the stage
-SquareStack pacman_AI2(SquarePos pos, Stage& stage)
+SquareStack pacman_AI2(int id, SquarePos pos, Stage& stage)
 {
     SquareStack stack;
     char input = stage.last_key_input;
@@ -201,7 +269,7 @@ SquareStack pacman_AI2(SquarePos pos, Stage& stage)
     return stack;
 }
 
-SquareStack pacman_AI(SquarePos pos, Stage& stage)
+SquareStack pacman_AI3(int id, SquarePos pos, Stage& stage)
 {
     SquareStack stack;
     SquarePos current_pos = pos;
@@ -242,6 +310,9 @@ SquareStack pacman_AI(SquarePos pos, Stage& stage)
         return stack;
     }
     
+    //stack.push({current_pos.first + line_movement, current_pos.second + column_movement});
+    //return stack;
+    
     SquareStack temp_stack;
     while (not stage.matrix[current_pos.first + line_movement][current_pos.second + column_movement].obstructed)
     {
@@ -264,5 +335,57 @@ SquareStack pacman_AI(SquarePos pos, Stage& stage)
     
     if (stack.size() == 1) //if there is a node in the way, preserve the chosen direction
         stage.last_key_input = 0;
+    return stack;
+}
+
+SquareStack pacman_AI(int id, SquarePos pos, Stage& stage)
+{
+    SquareStack stack;
+    SquarePos current_pos = pos;
+    
+    char input = stage.last_key_input;
+    int line_movement = 0, column_movement = 0;
+    
+    //calculate the position if the key press is followed
+    bool no_press = false;
+    switch (input)
+    {
+        case 'z':
+            line_movement = -1;
+            break;
+        case 'q':
+            column_movement = -1;
+            break;
+        case 's':
+            line_movement = 1;
+            break;
+        case 'd':
+            column_movement = 1;
+            break;
+            
+        default:
+            no_press = true;
+            break;
+    }
+    
+    
+    //if not going through a wall, return this square
+    if(not stage.matrix[current_pos.first + line_movement][current_pos.second + column_movement].obstructed)
+    {
+        stack.push({current_pos.first + line_movement, current_pos.second + column_movement});
+        return stack;
+    }
+    
+    //if we are here, try to go in the same direction as before
+    line_movement = current_pos.first - stage.entities[id].get_previous_square().first;
+    column_movement = current_pos.second - stage.entities[id].get_previous_square().second;
+    if(not stage.matrix[current_pos.first + line_movement][current_pos.second + column_movement].obstructed)
+    {
+        stack.push({current_pos.first + line_movement, current_pos.second + column_movement});
+        return stack;
+    }
+    
+    //if all else failed, stay still
+    stack.push({current_pos.first, current_pos.second});
     return stack;
 }
