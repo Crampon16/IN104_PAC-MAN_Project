bee436c47b8eeac2fff20844d255640465986274
Crampon16 <48726144+Crampon16@users.noreply.github.com>
2019-04-25 19:07:34 +0200

    Bfs comes back with its friends scatter, escape and death

--- Pac-man/pathfinding.hpp
+++ Pac-man/pathfinding.hpp
@@ -18,19 +18,32 @@
 
 struct Stage;
 
+//returns the shortest path between start and goal
+std::stack<std::pair<int, int>> bfs(Stage& stage, std::pair<int, int> start, std::pair<int, int> goal);
+
+
 //simply takes the shortest path to the player
-std::stack< std::pair<int, int> > blinky_AI(int id, std::pair<int, int> pos, Stage& stage);
+std::stack< std::pair<int, int> > blinky_AI(int id, Stage& stage);
 
 //heads towards the player's next node
-std::stack< std::pair<int, int> > pinky_AI(int id, std::pair<int, int> pos, Stage& stage);
+std::stack< std::pair<int, int> > pinky_AI(int id, Stage& stage);
 
 //heads towards the node nearest to the intersection of pac man's direction and blinky's direction
-std::stack< std::pair<int, int> > inky_AI(int id, std::pair<int, int> pos, Stage& stage);
+std::stack< std::pair<int, int> > inky_AI(int id, Stage& stage);
 
 //heads towards pac-man, or go away if too close of him
-std::stack< std::pair<int, int> > clyde_AI(int id, std::pair<int, int> pos, Stage& stage);
+std::stack< std::pair<int, int> > clyde_AI(int id, Stage& stage);
+
+//heads towards the scatter point of the entity
+std::stack<std::pair<int, int>> scatter_AI(int id, Stage& stage);
+
+//heads towards the point at the farthest from pac-man
+std::stack<std::pair<int, int>> escape_AI(int id, Stage& stage);
+
+//heads towards the spawn point of the entity, by going through walls
+std::stack<std::pair<int, int>> death_AI(int id, Stage& stage);
 
 //get last_input and update the position
-std::stack< std::pair<int, int> > pacman_AI(int id, std::pair<int, int> pos, Stage& stage);
+std::stack< std::pair<int, int> > pacman_AI(int id, Stage& stage);
 
 #endif /* pathfinding_hpp */
