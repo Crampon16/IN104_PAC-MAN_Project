4e8d668aaa5ddb6a5ca5cd239e104bee5b83c7ac
Crampon16 <48726144+Crampon16@users.noreply.github.com>
2019-05-20 00:12:25 +0200

    Added win condition and result of the game

--- Pac-man/level_handler.cpp
+++ Pac-man/level_handler.cpp
@@ -11,19 +11,20 @@
 
 using namespace std;
 
-void classic_level(string layout, SDL_Renderer* renderer, vector<LTexture*> const &textures, LBitmapFont& font)
+bool classic_level(string layout, SDL_Renderer* renderer, vector<LTexture*> const &textures, LBitmapFont& font)
 {
-
+    
     Stage stage = init_stage(layout);
-
-
+    
+    
     SDL_Event e;
     bool quit = false;
-
+    bool victory = false;
+    
     FPSCapper cap(60);
-
+    
     stage.killer_mode_start = 0;
-
+    
     while( !quit )
     {
         cap.start();
@@ -43,15 +44,19 @@ void classic_level(string layout, SDL_Renderer* renderer, vector<LTexture*> cons
         handle_collisions(stage);
         handle_AIs(stage);
         display(renderer, stage, font, textures);
-
+        
         if(stage.lives == -1)
             quit = true;
-
-        //cout << (stage.entities[1].state == AFRAID) << (stage.entities[1].state == DEAD) << endl;
-        //cout << (stage.entities[2].state == AFRAID) << (stage.entities[2].state == DEAD) << endl;
-
+        else if(stage.number_of_gums <= 0)
+        {
+            quit = true;
+            victory = true;
+        }
+        
         cap.cap();
     }
+    
+    return victory;
 }
 
 void handle_collisions(Stage& stage)
@@ -61,33 +66,35 @@ void handle_collisions(Stage& stage)
     if(stage.matrix[pac_pos.first][pac_pos.second].item == "gum")
     {
         stage.score += 10;
+        --stage.number_of_gums;
         stage.matrix[pac_pos.first][pac_pos.second].item = "";
     }
-
+    
     // pac/super-gum collision
     if(stage.matrix[pac_pos.first][pac_pos.second].item == "super_gum")
     {
         stage.score += 100;
-
+        --stage.number_of_gums;
+        
         //enter killer mode
         stage.entities[0].state = KILLER;
         stage.killer_mode_start = SDL_GetTicks();
-
+        
         for (int i = 1; i <= 4; ++i)
         {
             //put ghost in afraid state, make him flee pac, and reverse its current direction
             stage.entities[i].state = AFRAID;
-
+            
             pair<int, int> tmp = stage.entities[i].get_previous_square();
             stage.entities[i].set_previous_square(stage.entities[i].get_path().top());
-
+            
             stage.entities[i].set_path_finding(escape_AI);
             stage.entities[i].find_path(stage);
         }
-
+        
         stage.matrix[pac_pos.first][pac_pos.second].item = "";
     }
-
+    
     // pac/ghost collision
     for (int i = 1; i <= 4; ++i)
     {
@@ -95,7 +102,7 @@ void handle_collisions(Stage& stage)
         {
             switch (stage.entities[i].state)
             {
-                //in this case, pac man dies
+                    //in this case, pac man dies
                 case NORMAL:
                 {
                     stack<pair<int, int>> pac_path, blink_path, pink_path, inky_path, clyde_path;
@@ -104,8 +111,8 @@ void handle_collisions(Stage& stage)
                     pink_path.push({stage.entities_spawn_point[2]});
                     inky_path.push({stage.entities_spawn_point[3]});
                     clyde_path.push({stage.entities_spawn_point[4]});
-
-
+                    
+                    
                     stage.entities[0].set_position(stage.entities_spawn_point[0], stage);
                     stage.entities[0].set_path(pac_path);
                     stage.entities[1].set_position(stage.entities_spawn_point[1], stage);
@@ -116,32 +123,32 @@ void handle_collisions(Stage& stage)
                     stage.entities[3].set_path(inky_path);
                     stage.entities[4].set_position(stage.entities_spawn_point[4], stage);
                     stage.entities[4].set_path(clyde_path);
-
+                    
                     stage.last_key_input = ' ';
                     --stage.lives;
                     break;
                 }
-
-                //in this case, the ghost dies
+                    
+                    //in this case, the ghost dies
                 case AFRAID:
                 {
                     //put the ghost in the dead state and increase score
                     stage.entities[i].state = DEAD;
                     stage.entities[i].set_path(bfs(stage, stage.entities_positions[i], stage.entities_spawn_point[i]));
                     stage.entities[i].set_speed(200);
-
+                    
                     stage.score += 200;
                     break;
                 }
-
-                //nothing to do in this case yet
+                    
+                    //nothing to do in this case yet
                 case DEAD:
                 default:
                     break;
             }
-
-
-
+            
+            
+            
             break;
         }
     }
@@ -166,13 +173,13 @@ void handle_AIs(Stage& stage)
                 stage.entities[i].state = NORMAL;
                 stage.entities[i].set_path_finding(stage.normal_pathfinder[i]);
             }
-
+            
             //every 5s, ghosts enter chase mode for 15s, before scattering again for 5s and so on
             if (SDL_GetTicks() % 20000 < 5000)
                 stage.entities[i].set_path_finding(scatter_AI);
             else
                 stage.entities[i].set_path_finding(stage.normal_pathfinder[i]);
-
+            
         }
     }
 }
