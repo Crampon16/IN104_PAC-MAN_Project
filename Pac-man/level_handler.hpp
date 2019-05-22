c68287ebd2133b4b5c1cafd67a399159b7567604
Luckijig <48726156+Luckijig@users.noreply.github.com>
2019-05-22 17:58:37 +0200

    Update level_handler.hpp

--- Pac-man/level_handler.hpp
+++ Pac-man/level_handler.hpp
@@ -8,6 +8,7 @@
 #define level_handler_hpp
 
 #include <stdio.h>
+#include <SDL_mixer.h>
 
 #include <string>
 #include <vector>
@@ -17,8 +18,9 @@
 #include "labyrinth.hpp"
 #include "controller.hpp"
 #include "graphic_display.hpp"
+#include "constants.hpp"
 
-bool classic_level(std::string layout, SDL_Renderer* renderer, std::vector<LTexture*> const &textures, LBitmapFont& font);
+bool classic_level(std::string layout, SDL_Renderer* renderer, std::vector<LTexture*> const &textures, LBitmapFont& font, std::vector<Mix_Chunk*> const &sounds);
 
 /*
  This function detects several type of collisions and applies different effects to each:
@@ -27,6 +29,7 @@ bool classic_level(std::string layout, SDL_Renderer* renderer, std::vector<LText
  * ghost/pac collisions (out of killer mode) makes pac respawn and decreases the amount of lives remaining
  * ghost/pac collisions (in killer mode) increases the score and set ghost to their back to spawn behaviour
  */
-void handle_collisions(Stage& stage);
+void handle_collisions(Stage& stage, std::vector<Mix_Chunk*> const &sounds, SDL_Renderer* renderer, std::vector<LTexture*> const &textures, LBitmapFont& font);
 void handle_AIs(Stage& stage);
+void sound_animation(SDL_Renderer* renderer, std::vector<LTexture*> const &textures, LBitmapFont& font, Stage& stage, Mix_Chunk* const &sounds, Uint32 duration);
 #endif /* level_handler_hpp */
