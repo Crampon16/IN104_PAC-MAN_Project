7118b7bfded44726907c68d22bb4e774dc156f56
Crampon16 <48726144+Crampon16@users.noreply.github.com>
2019-05-22 08:49:04 +0200

    Added id of wall sprites

--- Pac-man/graphic_display.hpp
+++ Pac-man/graphic_display.hpp
@@ -2,9 +2,7 @@
 //  graphic_display.hpp
 //  Pac-man
 //
-//  Created by Liam Rampon on 03/04/2019.
-//  Copyright © 2019 Liam Rampon. All rights reserved.
-//
+
 
 #ifndef graphic_display_hpp
 #define graphic_display_hpp
@@ -17,7 +15,7 @@
 #include "constants.hpp"
 #include "labyrinth.hpp"
 
-const int AFRAID_ID = 5, BLINKING_ID = 6, DEAD_ID = 7;
+const int AFRAID_ID = 5, BLINKING_ID = 6, DEAD_ID = 7, WALL_ID = 8;
 
 void display(SDL_Renderer* renderer, Stage stage, LBitmapFont& font, std::vector<LTexture*> const &textures);
 
