3435442b659c37113aee21efe3c597b7cf188516
Crampon16 <48726144+Crampon16@users.noreply.github.com>
2019-05-23 00:05:51 +0200

    Header updated

--- Pac-man/level_handler.hpp
+++ Pac-man/level_handler.hpp
@@ -8,19 +8,22 @@
 #define level_handler_hpp
 
 #include <stdio.h>
-#include <SDL_mixer.h>
 
 #include <string>
 #include <vector>
+
+#include "SDL2_mixer/SDL_mixer.h"
 
 #include "texture_wrapper.hpp"
 #include "time_wrapper.hpp"
 #include "labyrinth.hpp"
 #include "controller.hpp"
 #include "graphic_display.hpp"
-#include "constants.hpp"
 
-bool classic_level(std::string layout, SDL_Renderer* renderer, std::vector<LTexture*> const &textures, LBitmapFont& font, std::vector<Mix_Chunk*> const &sounds);
+
+void display_splash(SDL_Renderer* renderer, LTexture* splash , Uint32 time, bool& quit);
+
+bool classic_level(std::string layout, SDL_Renderer* renderer, std::vector<LTexture*> const &textures, LBitmapFont& font, std::vector<Mix_Chunk*> const &sounds, bool& quit);
 
 /*
  This function detects several type of collisions and applies different effects to each:
@@ -29,7 +32,10 @@ bool classic_level(std::string layout, SDL_Renderer* renderer, std::vector<LText
  * ghost/pac collisions (out of killer mode) makes pac respawn and decreases the amount of lives remaining
  * ghost/pac collisions (in killer mode) increases the score and set ghost to their back to spawn behaviour
  */
-void handle_collisions(Stage& stage, std::vector<Mix_Chunk*> const &sounds, SDL_Renderer* renderer, std::vector<LTexture*> const &textures, LBitmapFont& font);
+
+void handle_collisions(Stage& stage, std::vector<Mix_Chunk*> const &sounds, SDL_Renderer* renderer, std::vector<LTexture*> const &textures, LBitmapFont& font, bool& quit);
 void handle_AIs(Stage& stage);
-void sound_animation(SDL_Renderer* renderer, std::vector<LTexture*> const &textures, LBitmapFont& font, Stage& stage, Mix_Chunk* const &sounds, Uint32 duration);
+
+void sound_animation(SDL_Renderer* renderer, std::vector<LTexture*> const &textures, LBitmapFont& font, Stage& stage, Mix_Chunk* const &sounds, Uint32 duration, bool& quit);
+
 #endif /* level_handler_hpp */
