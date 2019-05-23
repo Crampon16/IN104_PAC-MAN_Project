6620a7cfbad00c7ee5439daa368925283fdc65a0
Crampon16 <48726144+Crampon16@users.noreply.github.com>
2019-05-23 22:43:44 +0200

    Phase 4

--- Pac-man/MergeHead/boss_handler.hpp
+++ Pac-man/MergeHead/boss_handler.hpp
@@ -2,9 +2,7 @@
 //  boss_handler.hpp
 //  Pac-man
 //
-//  Created by Liam Rampon on 30/04/2019.
-//  Copyright © 2019 Liam Rampon. All rights reserved.
-//
+
 
 #ifndef boss_handler_hpp
 #define boss_handler_hpp
@@ -25,10 +23,10 @@ void boss_fight(SDL_Renderer* renderer, std::vector<LTexture*> const &textures, 
 
 void animation(SDL_Renderer* renderer, std::vector<LTexture*> const &textures, LBitmapFont& font, bool& quit);
 
-void move(Uint32 delta, Avatar& pac, char input, MergeHead& mhead, TetrisGrid& grid, PongBall& ball, PongRacket& r1, PongRacket& r2);
+void move(Uint32 delta, Avatar& pac, char input, MergeHead& mhead, TetrisGrid& grid, PongBall& ball, PongRacket& r1, PongRacket& r2, std::vector<Pellet>& pellets);
 
-void handle_collisions(Avatar& pac, MergeHead& mhead, TetrisGrid& grid, PongBall& ball, std::vector<Mix_Chunk*> const &sounds);
+void handle_collisions(Avatar& pac, MergeHead& mhead, TetrisGrid& grid, PongBall& ball, std::vector<Pellet>& pellets, std::vector<Mix_Chunk*> const &sounds);
 
-void display_stage(SDL_Renderer* renderer, Avatar& pac, MergeHead& mhead, TetrisGrid& grid, PongBall& ball, PongRacket& r1, PongRacket& r2, std::vector<LTexture*> const &textures, LBitmapFont& font);
+void display_stage(SDL_Renderer* renderer, Avatar& pac, MergeHead& mhead, TetrisGrid& grid, PongBall& ball, PongRacket& r1, PongRacket& r2, std::vector<Pellet>& pellets, std::vector<LTexture*> const &textures, LBitmapFont& font);
 
 #endif /* boss_handler_hpp */
